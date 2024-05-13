adbfa547f590a5226267a56efbaba703ec86a70b
Mana <mana94mobahi@gmail.com>
2024-05-13 17:44:52 +0330

    create luxury section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Find from "./components/find/Find";
 import Driver from "./components/driver/Driver";
+import Luxury from "./components/luxury/Luxury";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <Find/>
       <Driver/>
+      <Luxury/>
       
     </div>
   );
